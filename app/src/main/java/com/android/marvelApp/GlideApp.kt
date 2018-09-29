bbd15ef77b912a82f2c7804ccb7d2508ad849165
Rajat Beck <rajatmarcelb@gmail.com>
2018-09-29 12:32:40 +0530

    added glide module to show image of characters

---
+++ app/src/main/java/com/android/marvelApp/GlideApp.kt
@@ -0,0 +1,8 @@
+package com.android.marvelApp
+
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+open class GlideModule : AppGlideModule()
--- app/src/main/java/com/android/marvelApp/ui/viewholders/CharacterViewHolder.kt
+++ app/src/main/java/com/android/marvelApp/ui/viewholders/CharacterViewHolder.kt
@@ -1,20 +1,27 @@
 package com.android.marvelApp.ui.viewholders
 
-import android.support.annotation.StringRes
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import com.android.marvelApp.GlideApp
 import com.android.marvelApp.R
 import com.android.marvelApp.data.remote.Result
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.character_row_view.view.*
 
-public class CharacterViewHolder(parent: ViewGroup):RecyclerView.ViewHolder(
+public class CharacterViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.character_row_view, parent, false)
-){
-    fun bindTo(result:Result?){
+) {
+    fun bindTo(result: Result?) {
         result?.let {
+            GlideApp.with(itemView)
+                    .load("${it.thumbnail.path}.${it.thumbnail.extension}")
+                    .centerCrop()
+                    .optionalCircleCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                    .into(itemView.ivAvatar);
             itemView.tvCharacterName.text = it.name
+
         }
     }
 
